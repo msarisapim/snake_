e45f660002e9733770ae847adce2e1e033ff1dff
msarisapim <msarisapim@gmail.com>
2024-01-26 10:50:26 +0700

    try handtracking code

--- handtrack.py
+++ handtrack.py
@@ -1,6 +1,5 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('model\yolov8m.pt')  # build a new model from YAML
-result = model.track(source=0, show=True)
-# result = model(source=0, show=True)
+model = YOLO('model\yolov8n-pose.pt')  # build a new model from YAML
+result = model(source=0, show=True)
